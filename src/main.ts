7cedf405d15d79fda6ce79352284c7cc63d1c385
Malachi-H <malachi.haskew@icloud.com>
2025-06-09 13:17:53 +1000

    tried copying over pandoc functions, but I think I'll have to work out plugin dependency because the render functin is far too interconnected. I'll take another look at it thought because I only need to convert to HTML, not output as a pdf.

--- src/main.ts
+++ src/main.ts
@@ -8,28 +8,58 @@ import {
 	Plugin,
 	PluginSettingTab,
 	Setting,
+	FileSystemAdapter,
 } from "obsidian";
 import "@total-typescript/ts-reset";
 import "@total-typescript/ts-reset/dom";
 import { MySettingManager } from "@/SettingManager";
+
 export default class MyPlugin extends Plugin {
 	private eventRefs: EventRef[] = [];
 	async onload() {
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: "export-censored-document",
 			name: "Export Censored Document",
 			checkCallback: (checking: boolean) => {
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					if (!checking) {
-						// run code
+						this.startHTMLExport();
 					}
 					return true;
 				}
 			},
 		});
 	}
+
+	async startHTMLExport(): Promise<void> {
+		let inputFile = this.getCurrentFile();
+		if (!inputFile) {
+			new Notice("Could not determine the file path.");
+			return;
+		}
+		new Notice(`Exporting ${inputFile} to HTML`);
+		let outputFile: string = this.replaceFileExtension(inputFile, "html");
+        const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const { html, metadata } = await render(this, view, inputFile, format);
+	}
+
+	replaceFileExtension(file: string, ext: string): string {
+		// Source: https://stackoverflow.com/a/5953384/4642943
+		let pos = file.lastIndexOf(".");
+		return file.slice(0, pos < 0 ? file.length : pos) + "." + ext;
+	}
+
+	getCurrentFile(): string | null {
+		const fileData = this.app.workspace.getActiveFile();
+		if (!fileData) return null;
+		const adapter = this.app.vault.adapter;
+		if (adapter instanceof FileSystemAdapter)
+			return adapter.getFullPath(fileData.path);
+		return null;
+	}
+
 	onunload() {
 		super.onunload();
 		// unload all event ref
